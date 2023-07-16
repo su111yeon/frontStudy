59b1a88ff5958acea6c2f20e963c7817fa2e0121
yunchoun <chdns2000@naver.com>
2023-07-16 18:06:12 +0900

    todolist push

--- React/rt10.create-reate-project/reactprj01/src/App.js
+++ React/rt10.create-reate-project/reactprj01/src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-// import CompStyle from './containers/styled/CompStyle'
-import CrudContainer from './containers/crud/CrudContainer';
+import PageHome from './pages/PageHome';
 
 function App() {
   return (
     <div>
-      <CrudContainer></CrudContainer>
+      <PageHome></PageHome>
     </div>
   );
 }
